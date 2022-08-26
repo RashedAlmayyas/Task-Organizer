18e98063ca667f070c04d90da5b0fd82c57e398f
NOORshatnawi <108795086+NOORshatnawi@users.noreply.github.com>
2022-08-26 16:36:19 +0300

    Add files via upload

---
+++ js.js
@@ -0,0 +1,27 @@
+let sidebar = document.querySelector(".sidebar");
+let toggler = document.querySelector(".toggler");
+
+toggler.addEventListener("click", function () {
+  this.classList.toggle("open");
+  sidebar.classList.toggle("open");
+});
+
+const monthNameEl = document.getElementById("month-name");
+const dayNameEl = document.getElementById("day-name");
+const dayNumEl = document.getElementById("day-number");
+const yearEl = document.getElementById("year");
+
+const date = new Date();
+const month = date.getMonth();
+
+monthNameEl.innerText = date.toLocaleString("en", {
+  month: "long",
+});
+
+dayNameEl.innerText = date.toLocaleString("en", {
+  weekday: "long",
+});
+
+dayNumEl.innerText = date.getDate();
+
+yearEl.innerText = date.getFullYear();
